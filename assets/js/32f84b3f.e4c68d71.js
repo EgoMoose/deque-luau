32c5d617636aea299195ec23e44d2791b70d98e1
EgoMoose <EgoMoose@users.noreply.github.com>
2024-07-18 15:30:41 -0400

    Built and published by Moonwave

---
+++ assets/js/32f84b3f.e4c68d71.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[539],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/Deque","label":"\u200bDeque"}]')}}]);
---
+++ assets/js/c808a308.e1ce17eb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[417],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/EgoMoose/deque-luau/blob/main","baseUrl":"/deque-luau/","classOrder":[],"apiCategories":["Constructors","Methods"]}')}}]);
